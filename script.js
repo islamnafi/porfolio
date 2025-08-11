61966054d5a0956fe86e2ce07469121042942eea
Nafi <islamnafi99@gmail.com>
2025-08-12 02:15:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
   const PROMPT_PATH = "Portfolio:\\Nafi";
   const HINT_TEXT = "Type 'help' and press Enter";
   const BANNER_TEXT = [
-    "[Version 10.0.19045.6093]",
+    "[Version 10.0.19045.14]",
     "All rights reseseved by Nafiul Islam.",
     ""
   ].join("\n");
